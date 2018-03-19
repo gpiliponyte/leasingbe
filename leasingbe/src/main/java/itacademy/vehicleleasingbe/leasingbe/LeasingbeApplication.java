5f9f4698b3e45a3d36c6a0a6e9ca403f97cd20e4
Greta Piliponytė <g.piliponyte97@gmail.com>
2018-03-19 22:23:31 +0200

    first commit

---
+++ leasingbe/src/main/java/itacademy/vehicleleasingbe/leasingbe/LeasingbeApplication.java
@@ -0,0 +1,12 @@
+package itacademy.vehicleleasingbe.leasingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeasingbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeasingbeApplication.class, args);
+	}
+}
