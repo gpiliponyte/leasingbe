a1890af917c22e1cf0b247cc7c1edf1c707a2490
Greta Piliponytė <g.piliponyte97@gmail.com>
2018-04-07 21:28:49 +0300

    Update LeasingFormService.java

--- src/main/java/itacademy/vehicleleasingbe/leasingbe/services/LeasingFormService.java
+++ src/main/java/itacademy/vehicleleasingbe/leasingbe/services/LeasingFormService.java
@@ -48,7 +48,7 @@ public class LeasingFormService {
 
         leasingForm.setUniqueId(uniqueId);
         leasingForm.setDate(calendar.getTime());
-        leasingForm.setApplicationStatus('pending');
+        leasingForm.setApplicationStatus("pending");
 
         LeasingForm newLeasingForm = leasingForm;
         return leasingFormRepository.save(newLeasingForm);
