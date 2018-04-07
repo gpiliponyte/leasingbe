8827b85e4590e3e7cf42bb15a0a37e6945d0c83c
Greta Piliponytė <g.piliponyte97@gmail.com>
2018-04-07 22:25:48 +0300

    Update UniqueIdGeneratorService.java

--- src/main/java/itacademy/vehicleleasingbe/leasingbe/services/UniqueIdGeneratorService.java
+++ src/main/java/itacademy/vehicleleasingbe/leasingbe/services/UniqueIdGeneratorService.java
@@ -2,6 +2,7 @@ package itacademy.vehicleleasingbe.leasingbe.services;
 
 import itacademy.vehicleleasingbe.leasingbe.beans.documents.LeasingForm;
 import org.springframework.stereotype.Service;
+import java.util.Random;
 
 import java.util.*;
 
@@ -31,7 +32,8 @@ public class UniqueIdGeneratorService {
             return userId;
         } else if (leasingForm.getCustomerType().equals("Business")) {
             long timeNow = System.currentTimeMillis();
-            String timeStamp = Long.toString(timeNow).substring(0,3);
+            Random rnd = new Random();
+            int randomInteger = 1000+rnd.nextInt(9000);
             String threeCompanyCodeNumbers = leasingForm.getCompanyCode().substring(0, 4);
             String possibleChars = "abcdefghjklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
             StringBuilder stringBuilder = new StringBuilder();
@@ -41,7 +43,7 @@ public class UniqueIdGeneratorService {
                 stringBuilder.append(possibleChars.charAt(index));
             }
             String generatedString = stringBuilder.toString();
-            String userId = threeCompanyCodeNumbers +  generatedString + timeStamp;
+            String userId = threeCompanyCodeNumbers +  generatedString + randomInteger;
             System.out.println(userId);
             return userId;
         } else {
