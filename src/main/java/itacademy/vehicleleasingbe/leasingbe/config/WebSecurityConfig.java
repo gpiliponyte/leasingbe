0cb99805d4d8e98c4327165d6dedbfed540fe47d
Greta Piliponytė <g.piliponyte97@gmail.com>
2018-04-07 17:44:26 +0300

    fix

--- src/main/java/itacademy/vehicleleasingbe/leasingbe/config/WebSecurityConfig.java
+++ src/main/java/itacademy/vehicleleasingbe/leasingbe/config/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/token/*", "/login", "/addLease" , "/uniqueId/{uniqueId}",
+                .antMatchers("/token/*", "/signup", "/addLease" , "/uniqueId/{uniqueId}",
                         "/getCalendar", "/vehicles").permitAll()
                 .anyRequest().authenticated()
                 .and()
